50ce6335c33474ea66c150f92e85c733be985251
Grithisgaa A S <grithisgaa200806@gmail.com>
2026-09-12 10:53:46 +0530

    added

---
+++ w5a1.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class w5a1 {
+
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] answer = new int[n];
+
+        // Forward pass: product of elements to the left
+        int leftProduct = 1;
+        for (int i = 0; i < n; i++) {
+            answer[i] = leftProduct;
+            leftProduct *= nums[i];
+        }
+
+        // Backward pass: product of elements to the right
+        int rightProduct = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            answer[i] *= rightProduct;
+            rightProduct *= nums[i];
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+
+        int[] result = productExceptSelf(nums);
+
+        System.out.println("Input: " + Arrays.toString(nums));
+        System.out.println("Output: " + Arrays.toString(result));
+    }
+}
